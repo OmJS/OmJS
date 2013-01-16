edb43639ce86e3c42d2484f1ba19bfb91e4c9928
OmJS <live.Dron@gmail.com>
2013-01-16 01:37:07 -0800

    Update build_1_0_1.js

--- build_1_0_1.js
+++ build_1_0_1.js
@@ -10,6 +10,12 @@
     _init:Use to initialise the Bootloader Array
         params: a[Array] which contains the list of functions to be fired or it can be set to true in case
                 you require to reset the BootLoaders.
+    list:Array List having the list of functions and handlers to be fired
+    add:To add a new function or handler to the BootLoader.
+        param:f[function/object]: The parameter is directly pushed to the list.
+                In case of Object, the init function of the object is called when the BootLoaders are fired!
+    isSet: If not initialised then set to false, else true.
+
 */
 BootLoader={_init:function(a){
                     if(!BootLoader.isSet){
@@ -21,15 +27,16 @@ BootLoader={_init:function(a){
                         BootLoader.isSet=true;
                         }
                     }else{
-                        if(a)
-                        BootLoader.list=new Array();
+                        if(a){
+                            BootLoader.list=new Array();
+                        }
                         }
                     },
                     list:null,
                     add:function(f){
                         if(BootLoader.isSet){
-                            if(f typeof "function"|| f typeof "object"){
+                            if(f typeof "function" || f typeof "object"){
                                 BootLoader.list.push(f);
                                 }
-                            }
+                            },isSet:false
                             };
